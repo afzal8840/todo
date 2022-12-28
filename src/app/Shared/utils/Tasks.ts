d20d66d65f1277c6007431d30e2c3bccc2497d98
afzal8840 <ansariafzal4457@gmail.com>
2022-12-28 12:15:33 +0530

    todo

---
+++ src/app/Shared/utils/Tasks.ts
@@ -0,0 +1,44 @@
+
+export const Development  = [
+    {
+      title: 'Afzal',
+      description: 'Developer',
+    },
+    {
+      title: 'Shaban',
+      description: 'Developer',
+    },
+];
+
+export const InProgress = [
+    {
+      title: 'Fahad',
+      description: 'Developer',
+    },
+    {
+      title: 'Shabahat',
+      description: 'Developer',
+    },
+];
+
+export const Qa = [
+    {
+      title: 'Tariq-Bhai',
+      description: 'Developer',
+    },
+    {
+      title: 'Shamim',
+      description: 'Developer',
+    },
+];
+
+export const Production = [
+    {
+      title: 'Aamir',
+      description: 'Developer',
+    },
+    {
+      title: 'Farman-Bhai',
+      description: 'Developer',
+    },
+];
